237527113170ae43b8896b8abd49e5b1c8a1f4b4
miguellandeiropinto <miguel.pinto@m4yweb.com>
2017-10-17 00:08:03 +0100

    xcv

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api/v1/menssegerbot', function(req, res, next) {
+
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'abdce') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+
 });
 
 module.exports = router;
